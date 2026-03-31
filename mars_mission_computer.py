5b736bc56bb880697c94d850ac80769f91cad142
K-yoon03 <yoonnyang1013@gmail.com>
2026-03-31 15:50:59 +0900

    feat: mars_mission_computer.py implement

---
+++ mars_mission_computer.py
@@ -0,0 +1,31 @@
+import random
+
+class DummySensor:
+    def __init__(self): #generator
+        self.env_values = {
+            'mars_base_internal_temperature': 0.0,
+            'mars_base_external_temperature': 0.0,
+            'mars_base_internal_humidity': 0.0,
+            'mars_base_external_illuminance': 0.0,
+            'mars_base_internal_co2': 0.0,
+            'mars_base_internal_oxygen': 0.0
+        }
+    def EnvironSet(self):
+        self.env_values['mars_base_internal_temperature'] = round(random.uniform(18, 30), 2)
+        self.env_values['mars_base_external_temperature'] = round(random.uniform(0, 21), 2)
+        self.env_values['mars_base_internal_humidity'] = round(random.uniform(50, 60), 2)
+        self.env_values['mars_base_external_illuminance'] = round(random.uniform(500, 715), 2)
+        self.env_values['mars_base_internal_co2'] = round(random.uniform(0.02, 0.1), 4)
+        self.env_values['mars_base_internal_oxygen'] = round(random.uniform(4, 7), 2)
+
+    def getEnvironment(self): #getter Method
+        return self.env_values
+
+if __name__ == '__main__':
+    DummySensor = DummySensor()
+
+    DummySensor.EnvironSet()
+    CurrentEnvironment = DummySensor.getEnvironment()
+
+    for key, value in CurrentEnvironment.items():
+        print(f'{key} : {value}')
